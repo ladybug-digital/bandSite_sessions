b0c1fad376909439421bdcb019eca570a4d68e08
HrvojeSimanovic <hrvoje@ladybug.digital>
2023-02-14 11:09:36 +0100

    add search feat to music section with 'Fuse.js'

---
+++ assets/js/search.js
@@ -0,0 +1,72 @@
+import Fuse from 'fuse.js';
+import * as params from '@params';
+
+const searchBox = document.querySelector('#search input');
+const searchResults = document.getElementById('searchResults');
+const RESULTS_NUM = 5;
+
+const search = async () => {
+    try {
+        let response = await window.fetch(params.MUSIC_URL + 'index.json');
+            if (!response.ok) {
+                return;
+            }
+            let data = await response.json();
+            
+            const options = {
+                threshold: 0.3,
+                ignoreLocation: true,
+                keys: ['title', 'url', 'content', 'cover']
+            }
+
+            const fuseIndex = Fuse.createIndex(options.keys, data);
+            index = new Fuse(data, options, fuseIndex);
+
+            document.querySelector('#search input')?.addEventListener('input', showResult);
+            document.addEventListener('keydown', searchBoxFocus);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+showResult = (event) => {
+    const results = index.search(searchBox.value).slice(0, RESULTS_NUM);
+    if (results.length > 0) {
+        searchResults.innerHTML = results.map((el) => {
+            if (el.item.cover) {
+                return `<div>
+                            <a href="${el.item.url}">
+                            <b>${el.item.title}</b>
+                            <img src="${el.item.cover}" width="40" height="40">
+                            </a>
+                        </div>`
+            } else {
+                return `<div>
+                            <a href="${el.item.url}">
+                            <b>${el.item.title}</b>
+                            </a>
+                        </div>`
+            }
+        }).join('');
+    } else {
+        searchResults.innerHTML = '';
+    }
+}
+
+searchBoxFocus = (event) => {
+    const removeAllResults = (parent) => {
+        while (parent.firstElementChild) {
+            parent.removeChild(parent.firstElementChild);
+        }
+    }
+    if (event.ctrlKey && event.key === '/') {
+        searchBox?.focus();
+    } else if (event.key == 'Escape' && searchBox) {
+        searchBox.value = '';
+        removeAllResults(searchResults);
+        searchBox?.blur();
+    }
+}
+
+search();
--- content/music/_index.md
+++ content/music/_index.md
@@ -2,5 +2,7 @@
 title: "Music"
 date: 2023-01-23T11:46:30+01:00
 draft: false
+outputs:
+- html
+- json
 ---
-
